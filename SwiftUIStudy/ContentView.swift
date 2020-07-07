4a10f1ca42eaf4b591cd23a0e6d0b9c82770098a
iammiori <mioriii@naver.com>
2020-07-08 03:33:26 +0900

    [init] Init commit

---
+++ SwiftUIStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by miori Lee on 2020/07/08.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
